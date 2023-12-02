df1921cbace8ed72218cf2f08360f692abe33aef
danieljou <danielsitepro@gmail.com>
2023-12-02 10:28:33 +0100

    Assets

--- render/settings.py
+++ render/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tools')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 
 MEDIA_URL = '/media/'#
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
